7bfcef2462e4ba66e036e3026e283b83f83e51ea
Ray Veid <15350246+rayveid@users.noreply.github.com>
2023-01-25 15:13:45 +0530

    Implemented CompareJson function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import compareJson from '../src/compareJson';
+// import compareJson from '../src/compareJson';
 
 const genDiff = new Command();
 
--- src/compareJson.js
+++ src/compareJson.js
@@ -1,6 +1,36 @@
-import _ from 'lodash';
-import path from 'node:path';
+const mapping = {
+    1: '-', // значение есть только в первом файле ИЛИ они разные со вторым
+    2: '+', // значение есть только во втором файле ИЛИ они разные с первым
+    3: ' ', // значения есть в обоих файлах && равны
+}
 
-const filepath1 = path.join();
+const compareJson = (obj1, obj2) => {
 
-export default compareJson = (filepath1, filepath2) => {};
+    // создаем ентрис из объектов, для корректной сортировки добавляем вторым значением
+    // в массив номер объекта
+    const obj1Entries = Object.entries(obj1).map(([key, value]) => [key, 1, value]);
+    const obj2Entries = Object.entries(obj2).map(([key, value]) => [key, 2, value]);
+
+    // объединяем массивы и сортируем
+    const entries = [...obj1Entries, ...obj2Entries]
+        .sort() // сортируем
+        .reduce((acc, [key, obj, value]) => {
+            if (acc.length === 0) return [...acc, [key, obj, value]];  // если акк пустой или значения ключей не совпадают, сразу возвращаем его с новыми значениями
+            const prevItem = acc[acc.length - 1]; // достаем предыдущий элемент из акка
+            const [prevKey, , prevValue] = prevItem; // достаем значения из предыдущего
+            if (prevKey !== key) return [...acc, [key, obj, value]]; // если значения ключей  не совпадают, возвращаем его с новыми значениями
+            if (prevValue !== value) return [...acc, [key, obj, value]];  // если значения value не совпадают, возвращаем его с новыми значениями
+            const newPrevItem = [key, 3, value]; // обновляем предыдущее значения
+            const newAcc = acc.slice(0, acc.length - 1); // удаляем последнее значение из акка
+            return [...newAcc, newPrevItem]
+        }, [])
+        .reduce((result, [key, obj, value]) => {
+            const newProp = { [`${mapping[obj]} ${key}`]: value } // формируем новое свойство
+            return { ...result, ...newProp }; // добавляем его в результатт
+        }, {})
+
+    // редусим массив, собираем итоговый результат
+    return JSON.stringify(entries)
+};
+
+export default {compareJson}
