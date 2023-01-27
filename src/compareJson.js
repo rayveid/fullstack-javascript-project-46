03efbbb346ce65d72c5388da1e0efd4d8751d98d
Ray Veid <15350246+rayveid@users.noreply.github.com>
2023-01-27 12:59:07 +0530

    Updated compareJson module. Implemented recursive logic

--- src/compareJson.js
+++ src/compareJson.js
@@ -1,39 +1,10 @@
-const mapping = {
-    1: '-', // значение есть только в первом файле ИЛИ они разные со вторым
-    2: '+', // значение есть только во втором файле ИЛИ они разные с первым
-    3: ' ', // значения есть в обоих файлах && равны
-}
+import renderMap from './renderMap.js'
+import makeMap from './makeMap.js'
 
 // функция принимает на вход 2 json-объекта
 const compareJson = (obj1, obj2) => {
-
-    // создаем ентрис из объектов, для корректной сортировки добавляем вторым значением
-    // в массив номер объекта
-    const obj1Entries = Object.entries(obj1).map(([key, value]) => [key, 1, value]);
-    const obj2Entries = Object.entries(obj2).map(([key, value]) => [key, 2, value]);
-
-    // объединяем массивы и сортируем
-    return [...obj1Entries, ...obj2Entries]
-        .sort() // сортируем
-        .reduce((acc, [key, obj, value]) => {
-            if (acc.length === 0) return [...acc, [key, obj, value]];  // если акк пустой или значения ключей не совпадают, сразу возвращаем его с новыми значениями
-
-            const prevItem = acc[acc.length - 1]; // достаем предыдущий элемент из акка
-            const [prevKey, , prevValue] = prevItem; // достаем значения из предыдущего
-
-            if (prevKey !== key) return [...acc, [key, obj, value]]; // если значения ключей  не совпадают, возвращаем его с новыми значениями
-            if (prevValue !== value) return [...acc, [key, obj, value]];  // если значения value не совпадают, возвращаем его с новыми значениями
-
-            const newPrevItem = [key, 3, value]; // обновляем предыдущее значения
-            const newAcc = acc.slice(0, acc.length - 1); // удаляем последнее значение из акка
-
-            return [...newAcc, newPrevItem]
-        }, [])
-        .reduce((result, [key, obj, value]) => {
-            const newProp = { [`${mapping[obj]} ${key}`]: value } // формируем новое свойство
-
-            return { ...result, ...newProp }; // добавляем его в результат
-        }, {})
+    const equalityMap = makeMap(obj1, obj2);
+    return renderMap(equalityMap, obj1, obj2);
 };
 
 export default compareJson;
--- src/renderMap.js
+++ src/renderMap.js
@@ -1,6 +1,26 @@
+const stringify = (value, replacer = ' ', spacesCount = 2, depth = 1) => {
+    const type = typeof value;
+
+    if (type !== 'object') return value.toString(); // для всех кроме объектов, возвращаем в виде строки
+    if (value === null) return null; // для null (т.к. тоже объект) возвращаем null
+
+    const spaces = replacer.repeat(spacesCount * (depth + 1));
+    const bracketSpaces = replacer.repeat(spacesCount * (depth));
+
+    const result = Object.entries(value).reduce((acc, [entryKey, entryValue]) => {
+        const newKey = `${spaces}  ${entryKey.toString()}`;
+        const newValue = stringify(entryValue, replacer, spacesCount, depth + 1);
+
+        return [...acc, [`${newKey}: ${newValue}`]];
+
+    }, []);
+
+    return ['{', ...result, `${bracketSpaces}}`].join('\n'); // формируем и возращаем итоговую строку
+}
+
 const renderMap = (map, obj1, obj2, depth = 1) => {
-    const spaces = (' ').repeat(2 * depth);
-    const brackets = (' ').repeat(2 * (depth - 1));
+    const spaces = (' ').repeat(2*(depth)); // 4 - 5 - 6
+    const brackets = (' ').repeat(2*(depth - 1));
 
     // проходимся по всем элемента и собираемся объект из двух с помощью карты
     const mapEntry = Object
@@ -8,25 +28,28 @@ const renderMap = (map, obj1, obj2, depth = 1) => {
         .sort() // сортируем, т.к. итоговый результат должен быть отсортирован
         .reduce((acc, [key, value]) => {
 
-
             if (!Array.isArray(value)) { // рекурсивная логика
-                const arrValue = renderMap(map[key], obj1[key], obj2[key], depth + 1);
-                return [...acc, `${brackets}  ${key}: ${arrValue}`]
+                const arrValue = renderMap(map[key], obj1[key], obj2[key], depth + 2);
+                return [...acc, `${spaces}  ${key}: ${arrValue}`]
             }
 
             let newValue = [];
+            let strValue;
 
-            for (const valueKey of value) { // проходися по каждому значению из карты
+            for (const valueKey of value) { // проходимся по каждому значению из карты
 
                 switch (valueKey) {
-                    case 1:
-                        newValue = [...newValue, `${spaces}- ${key}: ${obj1[key]}`];
+                    case 1: // берем значение из первого объекта
+                        strValue = stringify(obj1[key], ' ', 2, depth + 1);
+                        newValue = [...newValue, `${spaces}- ${key}: ${strValue}`];
                         break;
-                    case 2:
-                        newValue = [...newValue, `${spaces}+ ${key}: ${obj2[key]}`];
+                    case 2: // берем значение из второго объекта
+                        strValue = stringify(obj2[key], ' ', 2, depth + 1);
+                        newValue = [...newValue, `${spaces}+ ${key}: ${strValue}`];
                         break;
-                    case 0:
-                        newValue = [...newValue, `${spaces}  ${key}: ${obj1[key]}`];
+                    case 0: // значения равны, можно взять из любого
+                        strValue = stringify(obj1[key], ' ', 2, depth + 1);
+                        newValue = [...newValue, `${spaces}  ${key}: ${strValue}`];
                         break;
                     default:
                         break;
@@ -34,8 +57,6 @@ const renderMap = (map, obj1, obj2, depth = 1) => {
             }
             return [...acc, ...newValue];
         }, [])
-
-    // console.log(mapEntry)
 
     return ['{', ...mapEntry, `${brackets}}`].join('\n');
 }
