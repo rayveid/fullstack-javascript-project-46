5e53844ac7c189933aec420d3df186b38286edd7
Ray Veid <15350246+rayveid@users.noreply.github.com>
2023-01-27 20:43:31 +0530

    updated gendiff program

--- gendiff.js
+++ gendiff.js
@@ -20,7 +20,7 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
     .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format', 'stylish') // опция для выбора формата вывода
+    .option('-f, --format <type>', 'output format') // опция для выбора формата вывода
     .action(genDiff);
 
 // запуск программы
